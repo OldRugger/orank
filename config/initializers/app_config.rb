c2828d228e865bf9eaf1de0bb3ada0e7d4bd66c9
Bob Frost <bob.a.frost@gmail.com>
2017-10-15 21:37:53 +0000

    bump version number

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,6 +1,6 @@
 APP_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/config.yml")).result)[Rails.env]
 APP_CONFIG.symbolize_keys!
 BUILD = `git rev-parse --short HEAD`
-APP_VERSION = '2.0.0'
+APP_VERSION = '2.1.0'
 COURSES = ['Red', 'Green', 'Brown', 'Orange', 'Yellow', 'Sprint'].freeze
 FINAL_SPLIT = 999
